181cb436f367fcabcb9506c5fc94a8c6779499f7
ZhiangChen <zxc251@case.edu>
2020-09-14 09:58:44 -0700

    added a model

--- README.md
+++ README.md
@@ -3,5 +3,19 @@ Precariously balanced rocks in gazebo simulation
 
 ---
 ```
-export GAZEBO_MODEL_PATH=/root/catkin_ws/src/pbr_gazebo/models${GAZEBO_MODEL_PATH:+:${GAZEBO_MODEL_PATH}}$
+export GAZEBO_MODEL_PATH=~/catkin_ws/src/pbr_gazebo/models${GAZEBO_MODEL_PATH:+:${GAZEBO_MODEL_PATH}}$
 ```
+
+1. coefficient of friction: 0.6
+
+2. terrain motion (DoF): 3 DoF 
+
+3. amplitude and frequency: 
+    acceleration 1~1.5 g, 1g (+); 
+    single stroke;
+    displacement 1m;
+    velocity 2m/s; (-)
+    
+4. density: 2700 kg/m^3
+
+5. Elasticity (--)
--- src/simple_box_sin_motion.py
+++ src/simple_box_sin_motion.py
@@ -14,14 +14,19 @@ if __name__ == '__main__':
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     HZ = 30
     rate = rospy.Rate(HZ)  # controller publishing rate
-    A = 1  # amplitude (meters)
-    T = 0.3  # period (seconds). note this distinguishes from controller publishing rate
+    A = 8  # amplitude (meters)
+    T = 0.5  # period (seconds). note this distinguishes from controller publishing rate
     t = 0  # initial horizon
     step = 1./HZ*(2*np.pi)/T
-    while not rospy.is_shutdown():
-        x = np.sin(t)
+    for _ in range(int(HZ*T)*4):
+        x = A*np.sin(t)
         vel = Twist()
         vel.linear.x = x
         pub.publish(vel)
         rate.sleep()
         t = t + step
+
+    for _ in range(10):
+        vel = Twist()
+        pub.publish(vel)
+        rate.sleep()
