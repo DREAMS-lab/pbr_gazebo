2a7d1f8037a12c489b4058840ab251c43b79c296
ZhiangChen <zxc251@case.edu>
2020-12-23 11:26:59 -0700

    fixed linear space sampling and shake table

--- src/pulse_motion_smart_client.py
+++ src/pulse_motion_smart_client.py
@@ -54,10 +54,15 @@ class SmartClient(object):
 
         rospy.loginfo('pluse_motion_smart_client has been initialized')
 
-        plt.axis([0, 3, 0, 2])
+        plt.axis([0, 1.2, 0, 1.2])
         self.toppling_data = []
 
         rospy.loginfo('Start simulation!')
+
+        FA_data = self.sampleMotionParam()
+        print(FA_data)
+
+        """        
         As = np.linspace(0.1, 0.5, 20)
         Fs = np.linspace(0.1, 1.5, 20)
         r = self.getRange(As, Fs)
@@ -65,38 +70,12 @@ class SmartClient(object):
         rospy.loginfo("Maximum PGA: " + str(r[0]))
         rospy.loginfo("Maximum PGV/PGA: " + str(r[2]))
         for A in As:
-            for F in Fs:
-                # load model
-                initial_pose = Pose()
-                initial_pose.position.x = 0
-                initial_pose.position.y = 0
-                initial_pose.position.z = 4
-                file_name = os.path.join(self.pkg_path, "models/rock_models/rock_box_1_1_2/model.sdf")
-                model_name = 'rock_box_1_1_2'
-                with open(file_name) as xml_file:
-                    sdf_f = xml_file.read()
-                self.load_client(model_name, sdf_f, '', initial_pose, "world")
-                rospy.sleep(2.)
+           for F in Fs:
+        """
 
-                # pulse motion
-                goal = pbr_gazebo.msg.AFGoal(A=A, F=F)
-                self.client.send_goal(goal)
-                self.client.wait_for_result()
-                result = self.client.get_result()
-                rospy.sleep(2.)
+        for F, A in FA_data:
+            self.runExperiment(A, F)
 
-                # delete model
-                self.delete_client('rock_box_1_1_2')
-
-                # reset shake table
-                goal = pbr_gazebo.msg.AFGoal(A=0, F=0)
-                self.client.send_goal(goal)
-                self.client.wait_for_result()
-                result = self.client.get_result()
-                rospy.sleep(2.)
-
-                state = self.checkToppled()
-                self.logData(A, F, state)
         nd_data = np.asarray(self.toppling_data)
         np.savetxt("toppling_data.csv", nd_data, delimiter=",")
 
@@ -107,6 +86,39 @@ class SmartClient(object):
             idx = data.name.index('rock_box_1_1_2')
             self.pbr_pose = data.pose[idx]
             self.pbr_twist = data.twist[idx]
+
+    def runExperiment(self, A, F):
+        # load model
+        initial_pose = Pose()
+        initial_pose.position.x = 0
+        initial_pose.position.y = 0
+        initial_pose.position.z = 4
+        file_name = os.path.join(self.pkg_path, "models/rock_models/rock_box_1_1_2/model.sdf")
+        model_name = 'rock_box_1_1_2'
+        with open(file_name) as xml_file:
+            sdf_f = xml_file.read()
+        self.load_client(model_name, sdf_f, '', initial_pose, "world")
+        rospy.sleep(2.)
+
+        # pulse motion
+        goal = pbr_gazebo.msg.AFGoal(A=A, F=F)
+        self.client.send_goal(goal)
+        self.client.wait_for_result()
+        result = self.client.get_result()
+        rospy.sleep(2.)
+
+        # delete model
+        self.delete_client('rock_box_1_1_2')
+
+        # reset shake table
+        goal = pbr_gazebo.msg.AFGoal(A=0, F=0)
+        self.client.send_goal(goal)
+        self.client.wait_for_result()
+        result = self.client.get_result()
+        rospy.sleep(2.)
+
+        state = self.checkToppled()
+        self.logData(A, F, state)
 
     def checkToppled(self):
         q = self.pbr_pose.orientation
@@ -144,7 +156,18 @@ class SmartClient(object):
         nd = np.asarray(data)
         return (nd[:, 0].max(), nd[:, 0].min(), nd[:, 1].max(), nd[:, 1].min())
 
+    def sampleMotionParam(self):
+        PGV_2_PGA = np.linspace(0.05, 1., 4)
+        PGA = np.linspace(0.05, 1., 4)
+        Fs = 1./(2*pi*PGV_2_PGA)
+        FA_data = []
+        for F in Fs:
+            for pga in PGA:
+                A = 9.807*pga/(4*pi**2*F**2)
+                FA_data.append((F, A))
 
+        FA_data = np.asarray(FA_data)
+        return FA_data
 
 
 if __name__ == '__main__':
