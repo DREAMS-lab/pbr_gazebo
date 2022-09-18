527b57581f5c391f673311232fcb1deeeb62688e
ZhiangChen <zxc251@case.edu>
2022-09-17 17:44:15 -0700

    edited param

--- src/pulse_motion_smart_client_double_rock_on_site.py
+++ src/pulse_motion_smart_client_double_rock_on_site.py
@@ -210,7 +210,7 @@ class SmartClient(object):
         return (nd[:, 0].max(), nd[:, 0].min(), nd[:, 1].max(), nd[:, 1].min())
 
     def sampleMotionParam(self):
-        PGV_2_PGA = np.linspace(0.2, 1., 50)
+        PGV_2_PGA = np.linspace(0.02, 1., 50)
         PGA = np.linspace(0.02, 1., 50)
         Fs = 1./(2*pi*PGV_2_PGA)
         FA_data = []
